5ffad7da1a1410d3358a4d0f44f47a0176b6a60d
Rubén Saiz Serrano <42714332+MiYazJE@users.noreply.github.com>
2019-04-10 00:29:44 +0200

    Jugando al Buscaminas

--- p315.java
+++ p315.java
@@ -1,148 +1,121 @@
-import java.util.*;
+    import java.util.*;
 
-public class p315 {
+    public class p315 {
 
-	public static void resuelveCaso(int[][] ref, int i, int j) {
-		int[] posF = {1, -1, 0, 0, 1, -1, -1, 1};
-		int[] posC = {0, 0, 1, -1, 1, -1, 1, -1};
-		int f, c;
-		for (int k = 0; k < 8; k++) {
-			f = i + posF[k];
-			c = j + posC[k];
-			if (valido(ref, f, c)) {
-				ref[f][c]++;
-			}
-		}
-	}
+        public static void resuelveCaso(int[][] ref, int i, int j) {
+            int[] posF = {1, -1, 0, 0, 1, -1, -1, 1};
+            int[] posC = {0, 0, 1, -1, 1, -1, 1, -1};
+            int f, c;
+            for (int k = 0; k < 8; k++) {
+                f = i + posF[k];
+                c = j + posC[k];
+                if (valido(ref, f, c)) {
+                    ref[f][c]++;
+                }
+            }
+        }
 
-	public static int buscaminas(int[][] ref, int f, int c) {
+        public static int buscaminas(int[][] ref, int f, int c) {
 
-		if (valido(ref, f, c) && !aux[f][c]) {
-			aux[f][c] = true;
-			//			imprimir(aux);
-			if (ref[f][c] != 0) { 
+            if (valido(ref, f, c) && !aux[f][c]) {
+                aux[f][c] = true;
+                if (ref[f][c] != 0) { 
 
-			} else {
-				return buscaminas(ref, f-1, c)   +  // arriba
-						buscaminas(ref, f-1, c+1) +  // arriba derecha
-						buscaminas(ref, f, c+1)   +  // derecha
-						buscaminas(ref, f+1, c+1) +  // derecha abajo
-						buscaminas(ref, f+1, c)   +  // abajo
-						buscaminas(ref, f+1, c-1) +  // izquierda abajo
-						buscaminas(ref, f, c-1)   +  // izquierda
-						buscaminas(ref, f-1, c-1);   // izquierda arriba
-			}
-		}
+                } else {
+                    return buscaminas(ref, f-1, c)   +  // arriba
+                            buscaminas(ref, f-1, c+1) +  // arriba derecha
+                            buscaminas(ref, f, c+1)   +  // derecha
+                            buscaminas(ref, f+1, c+1) +  // derecha abajo
+                            buscaminas(ref, f+1, c)   +  // abajo
+                            buscaminas(ref, f+1, c-1) +  // izquierda abajo
+                            buscaminas(ref, f, c-1)   +  // izquierda
+                            buscaminas(ref, f-1, c-1);   // izquierda arriba
+                }
+            }
 
-		return 0;
-	}
+            return 0;
+        }
 
-	public static boolean valido(int[][] m, int c, int f) {
-		if ((c >= 0 && c < m[0].length) && (f >= 0 && f < m.length))
-			return true;
+        public static boolean valido(int[][] m, int f, int c) {
+            if ((f >= 0 && f < m.length) && (c >= 0 && c < m[0].length))
+                return true;
 
-		return false;
-	}
+            return false;
+        }
 
-	public static void imprimirFinal(int[][] m) {
-		for (int i = 0; i < m.length; i++) {
-			for (int j = 0; j < m[0].length; j++) {
-				if (!aux[i][j]) {
-					System.out.print("X");
-				}
-				else if (aux[i][j] && m[i][j] == 0) {
-					System.out.print("-");
-				}
-				else {
-					System.out.print(m[i][j]);
-				}
-			}
-			System.out.println("");
-		}
+        public static void imprimirFinal(int[][] m) {
+            for (int i = 0; i < m.length; i++) {
+                for (int j = 0; j < m[0].length; j++) {
+                    if (!aux[i][j]) {
+                        System.out.print("X");
+                    }
+                    else if (aux[i][j] && m[i][j] == 0) {
+                        System.out.print("-");
+                    }
+                    else {
+                        System.out.print(m[i][j]);
+                    }
+                }
+                System.out.println("");
+            }
+        }
 
+        public static boolean[][] aux;
 
-	}
+        public static void main(String[] args) {
 
-	public static void imprimir(int[][] m) {
-		System.out.println("");
-		for (int i = 0; i < m.length; i++) {
-			for (int j = 0; j < m[0].length; j++) {
-				System.out.print(m[i][j] + " ");
-			}
-			System.out.println("");
-		}
-		System.out.println("\n");
-	}
- 
-	public static void imprimir(boolean[][] m) {
-		System.out.println("");
-		for (int i = 0; i < m.length; i++) {
-			for (int j = 0; j < m[0].length; j++) {
-				System.out.print((!m[i][j]) ? "0 " : "1 ");
-			}
-			System.out.println("");
-		}
-		System.out.println("\n");
-	}
+            Scanner s = new Scanner(System.in);
 
-	public static boolean[][] aux;
+            int f, c, casos, fBuscar, cBuscar;
+            int[][] ref;
+            String linea;
+            boolean gameOver;
 
-	public static void main(String[] args) {
+            while (s.hasNext()) {
 
-		Scanner s = new Scanner(System.in);
+                f = s.nextInt();
+                c = s.nextInt();
 
-		int f, c, casos, fBuscar, cBuscar;
-		int[][] ref;
-		String linea;
-		boolean gameOver;
+                ref = new int[f][c];
+                aux = new boolean[f][c];
+                
+                for (int i = 0; i < f; i++) {
+                    linea = s.next();
+                    for (int j = 0; j < c; j++) {
+                        if (linea.charAt(j) == '*') {
+                            ref[i][j] = -1;
+                            resuelveCaso(ref, i, j);
+                        }
+                    }
+                }
 
-		while (s.hasNext()) {
+                casos = s.nextInt();
+                gameOver = false;
 
-			f = s.nextInt();
-			c = s.nextInt();
+                for (int i = 0; i < casos; i++) {
 
-			ref = new int[f][c];
-			aux = new boolean[f][c];
+                    fBuscar = s.nextInt();
+                    cBuscar = s.nextInt();
+                    fBuscar--; cBuscar--;
 
-			for (int i = 0; i < f; i++) {
-				linea = s.next();
-				for (int j = 0; j < c; j++) 
-					if (linea.charAt(j) == '*') {
-						ref[i][j] = -1;
-						resuelveCaso(ref, i, j);
-					}
-			}
+                    if (ref[fBuscar][cBuscar] == -1) {
+                        gameOver = true;
+                    }
 
-			//			imprimir(ref);
+                    if (!gameOver) {
+                        buscaminas(ref, fBuscar, cBuscar);
+                    }
 
-			casos = s.nextInt();
-			gameOver = false;
+                }
 
-			for (int i = 0; i < casos; i++) {
+                if (gameOver) {
+                    System.out.println("GAME OVER");
+                } else {
+                    imprimirFinal(ref);
+                }
 
-				fBuscar = s.nextInt();
-				cBuscar = s.nextInt();
-				fBuscar--; cBuscar--;
+            }
 
-				if (ref[fBuscar][cBuscar] == -1) {
-					gameOver = true;
-				}
+        }
 
-				if (!gameOver) {
-					buscaminas(ref, fBuscar, cBuscar);
-				}
-				//				imprimir(aux);
-
-			}
-
-			if (gameOver) 
-				System.out.println("GAME OVER");
-			else 
-				imprimirFinal(ref);
-
-
-		}
-
-	}
-
-}
+    }
