a14775155148bdf37c718d3f8c70bede8764aaff
Rubén Saiz Serrano <42714332+MiYazJE@users.noreply.github.com>
2019-07-16 12:07:14 +0200

    Nana al bebé de papá y mamá

--- p417.java
+++ p417.java
@@ -2,78 +2,32 @@ import java.util.Scanner;
 
 public class p417 {
 
-	public static int devolverCiclica(String palabra) {
+    public static void main(String[] args) {
 
-		String ciclica  = "";
-		String comparar = "";
-		boolean esCiclica = true;
-		char letra = palabra.charAt(0);
-		boolean igual = true;
+        Scanner s = new Scanner(System.in);
 
-		for (int i = 1; i < palabra.length(); i++) {
-			if (palabra.charAt(i) != letra)  {
-				igual = false;
-				break;
-			}
-		}
-		
-		
-		if (igual == false) {
-			
-			for (int i = 0; i < palabra.length()/2; i++) {
+        String palabra;
+        boolean ciclica;
+        int size, pos;
 
-				esCiclica = true;
-				ciclica += palabra.charAt(i);
+        while (s.hasNext()) {
 
-				for (int j = i+1; j < palabra.length(); j+=i+1) {
+            palabra = s.next().toLowerCase();
+            size = 0;
+            ciclica = true;
 
-					if (j+i+1 > palabra.length()) {
-						esCiclica = false;
-						break;
-					}
-					
-					comparar = palabra.substring(j, j+ciclica.length());
+            for (int i = 0; i < palabra.length()/2; i++) {
+                ciclica = true; pos = 0;
+                for (int j = i+1; j < palabra.length(); j++) {
+                    if (palabra.charAt(pos) != palabra.charAt(j)) { ciclica = false; break;}
+                    pos++; pos %= i+1;
+                }
+                if (ciclica) { size = i+1; break; }
+            }
 
-					if (!ciclica.equals(comparar)) {
-						esCiclica = false;
-						break;
-					}
+            System.out.println( (ciclica && size>0 && palabra.length()%size==0) ? size : palabra.length() );
+        }
 
-				}
-				
-				if (esCiclica) break;
-
-			}
-			
-		} else {
-			return 1;
-		}
-
-		if (esCiclica) {
-			return ciclica.length();
-		} else {
-			return palabra.length();
-		}
-
-	}
-
-
-	public static void main(String[] args ) {
-
-		Scanner s = new Scanner(System.in);
-
-		String palabra;
-
-		while (s.hasNext()) {
-
-			palabra = s.next();
-			
-			palabra = palabra.toLowerCase();
-
-			System.out.println( devolverCiclica(palabra) );
-
-		}
-
-	}
+    }
 
 }
