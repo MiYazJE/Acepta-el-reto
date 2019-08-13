3b4adb6123ab16640bf7092093efc868cdfed837
Rubén Saiz Serrano <42714332+MiYazJE@users.noreply.github.com>
2019-08-13 17:50:22 +0200

    Conjuritis

--- p478.java
+++ p478.java
@@ -1,53 +1,116 @@
-import java.util.*;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class p478 {
 
     public static void main(String[] args) {
 
-        Scanner s = new Scanner(System.in);
+        final FastIO r = new FastIO(System.in);
+        int[] hechizos = new int[200_000];
+        long suma, target;
+        int c, left, right;
 
-        int nHechizos;
-        long[] hechizos;
-        int pDer;
-        int pIzq;
-        long suma, target, sum, danyo;
-        boolean found;
-        
         while (true) {
 
-        	nHechizos = s.nextInt();
-        	if (nHechizos == 0) break;
-        	
-            hechizos = new long[nHechizos];
+            c = r.readInt(); if (c == 0) break;
+
             suma = 0L;
-
-            for (int i = 0; i < nHechizos; i++) {
-                hechizos[i] = s.nextLong(); 
-                suma += hechizos[i];
-            }
-            danyo = s.nextLong();
-
-            pIzq = 0;
-            pDer = nHechizos-1;
-            found = false;
-            target = suma - danyo;
-            
-            while (!found) {
-
-            	sum = hechizos[pIzq] + hechizos[pDer];
-            	if (sum == target) found = true;
-            	else if (sum > target) {
-                    pDer--;
-                }
-                else {
-                    pIzq++;
-                }
-
+            for (int i = 0; i < c; i++) {
+                hechizos[i] = r.readInt(); suma += hechizos[i];
             }
 
-            System.out.print(hechizos[pIzq] + " " + hechizos[pDer] + "\n");
+            target = suma - r.readLong();
+
+            left = 0; right = c - 1;
+            while (hechizos[left] + hechizos[right] != target)
+                if (hechizos[left] + hechizos[right] > target) right--;
+                else left++;
+
+            System.out.println(hechizos[left] + " " + hechizos[right]);
         }
 
+    }
+
+}
+
+class FastIO {
+    private InputStream stream;
+    private byte[] buf = new byte[1024];
+
+    private int curChar;
+
+    private int numChars;
+
+    public FastIO(InputStream stream) {
+        this.stream = stream;
+    }
+
+    public int read() {
+        if (numChars == -1)
+            throw new RuntimeException();
+        if (curChar >= numChars) {
+            curChar = 0;
+            try {
+                numChars = stream.read(buf);
+            } catch (IOException e) {
+                throw new RuntimeException();
+            }
+            if (numChars <= 0)
+                return -1;
+        }
+        return buf[curChar++];
+    }
+
+    public String readString() {
+        final StringBuilder stringBuilder = new StringBuilder();
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        do {
+            stringBuilder.append((char) c);
+            c = read();
+        } while (!isSpaceChar(c));
+        return stringBuilder.toString();
+    }
+
+    public int readInt() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        int res = 0;
+        do {
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    public long readLong() {
+        int c = read();
+        while (isSpaceChar(c))
+            c = read();
+        int sgn = 1;
+        if (c == '-') {
+            sgn = -1;
+            c = read();
+        }
+        long res = 0;
+        do {
+            res *= 10;
+            res += c - '0';
+            c = read();
+        } while (!isSpaceChar(c));
+        return res * sgn;
+    }
+
+    public boolean isSpaceChar(int c) {
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
     }
 
 }
