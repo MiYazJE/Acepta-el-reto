ae9de96c0b6ee6fb8e1d22faf485eace8e3c4c6b
Rubén Saiz Serrano <42714332+MiYazJE@users.noreply.github.com>
2019-11-01 12:56:45 +0100

    Create p375.java

---
+++ p375.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+/**
+ * @author Rubén Saiz
+ */
+
+public class p375 {
+
+    public static void main(String[] args) {
+
+        final Scanner s = new Scanner(System.in);
+
+        String pista;
+        int num;
+        Character letra;
+
+        while (s.hasNext()) {
+
+            pista = s.next();
+
+            num = Integer.parseInt(pista.substring(0, 2));
+            letra = (pista.length() == 3) ? pista.charAt(2) : null;
+
+            num = (18 + num) % 36;
+            if (num == 0) num = 36;
+            System.out.printf("%02d", num);
+
+            if (letra != null) {
+                if (letra == 'R')      System.out.print("L");
+                else if (letra == 'L') System.out.print("R");
+                else                   System.out.print("C");
+            }
+
+            System.out.println();
+        }
+
+    }
+
+}
