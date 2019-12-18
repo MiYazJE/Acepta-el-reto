a63957148e160cc8b98fd11113edf12bf26c7764
Rubén Saiz Serrano <42714332+MiYazJE@users.noreply.github.com>
2019-12-19 00:03:49 +0100

    Desembalse

---
+++ p545.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class p545 {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int casos, max, res;
+        int[] arr;
+
+        while (true) {
+
+            casos = s.nextInt();
+            if (casos == 0) break;
+
+            arr = new int[casos];
+
+            max = 0;
+            for (int i = 0; i < casos; i++) {
+                arr[i] = s.nextInt();
+                max = Math.max(max, arr[i]);
+            }
+
+            res = 0;
+            for (int i = 0; i < casos; i++) {
+                res += Math.abs(max - arr[i]);
+            }
+
+            System.out.println( res );
+        }
+
+    }
+
+}
