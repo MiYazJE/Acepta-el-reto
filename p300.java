2cb991eba66c4c8653d5611c0281f45f299e2546
Rubén Saiz Serrano <42714332+MiYazJE@users.noreply.github.com>
2020-10-11 16:04:53 +0200

    Palabras pentavocálicas

--- p300.java
+++ p300.java
@@ -1,73 +1,24 @@
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class p300 {
 
+    static boolean isPentaVowel(String w) {
+        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        for (Character c : w.toCharArray()) vowels.remove(c);
+        return vowels.isEmpty();
+    }
 
-	public static boolean pentavocalica(String palabra) {
+    public static void main(String[] args) {
 
-		boolean pentavocalica = true;
-		char[] vocales = {'a', 'e', 'i', 'o', 'u'};
-		char letra;
+        Scanner s = new Scanner(System.in);
+        int C = s.nextInt();
 
-		for(int i = 0; i < palabra.length(); i++) {
+        while (C-- != 0)
+            System.out.println( isPentaVowel(s.next()) ? "SI" : "NO" );
 
-			letra = palabra.charAt(i);
-
-			if(     (letra == 'a') ||
-					(letra == 'e') ||
-					(letra == 'i') ||
-					(letra == 'o') ||
-					(letra == 'u')) {
-				
-				for(int j = 0; j < vocales.length; j++) {
-
-					if(letra == vocales[j]) {
-						vocales[j] = 'x';
-						break; 
-					}
-
-				}
-				
-			}
-			
-		}
-
-		for(int i = 0; i < vocales.length; i++) {
-
-			if(vocales[i] != 'x') {
-				pentavocalica = false;
-				return pentavocalica;
-			}
-
-		}
-
-		return pentavocalica;
-
-	}
-
-
-	public static void main(String[] args) {
-
-		Scanner s = new Scanner(System.in);
-
-		int casos = s.nextInt();
-		String palabra;
-
-		for(int i = 0; i < casos; i++) {
-
-			palabra = s.next();
-
-			if(pentavocalica(palabra)) {
-				System.out.println("SI");
-			} else {
-				System.out.println("NO");
-			}
-			
-		}
-
-		s.close();
-		System.exit(0);
-
-	}
+    }
 
 }
