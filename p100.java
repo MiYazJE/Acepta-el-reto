c96b548588cdf8c091c544b9763751c3c45a6c3d
Rubén Saiz Serrano <42714332+MiYazJE@users.noreply.github.com>
2020-10-14 04:19:06 +0200

    Constante de Kaprekar

--- p100.java
+++ p100.java
@@ -1,144 +1,69 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class p100 {
 
-	public static void main(String[] args) {
+    public static int KAPREKAR = 6174;
 
-		Scanner s = new Scanner(System.in); 
+    public static int ordAsc(char[] num) {
+        Arrays.sort(num);
+        return Integer.parseInt(new String(num));
+    }
 
-		int casos, num;
-		int iteraciones;
-		int kapkrekar, x, y;
-		
+    public static int ordDesc(char[] num) {
+        Arrays.sort(num);
+        reverse(num);
+        int n = Integer.parseInt(new String(num));
+        return (n / 1000 == 0) ? multiplyNum(n) : n;
+    }
 
-		casos = s.nextInt();
+    public static int multiplyNum(int num) {
+        if (num != 0 && num < 1000) return multiplyNum(num * 10);
+        return num;
+    }
 
-		for(int i=0; i<casos; i++) {
+    public static void reverse(char[] num) {
+        for (int i = 0; i < num.length / 2; i++) {
+            char temp = num[num.length - i - 1];
+            num[num.length - i - 1] = num[i];
+            num[i] = temp;
+        }
+    }
 
-			iteraciones = 1;
+    public static void main(String[] args) {
 
-			num = s.nextInt();
+        Scanner s = new Scanner(System.in);
 
-			// si el numero ya es la constante kapkrekar imprimimos 0
-			if(num == 6174) {
+        char[] num;
+        int C = s.nextInt();
 
-				iteraciones = 0;
+        while (C-- != 0) {
 
-			} else if(num == 0) {
+            num = s.next().toCharArray();
 
-				// si el numero es 0 directamente imprimiremos 8 porque es como un repdigit
-				iteraciones = 8;
+            if (Integer.parseInt(new String(num)) == KAPREKAR) {
+                System.out.println( 0 );
+                continue;
+            }
 
-			} else {
+            int iterations = 1;
+            while (iterations < 8) {
 
-				while(num < 1000) {
-					num *= 10;
-				}
+                int desc = ordDesc(num);
+                int asc  = ordAsc(num);
+                int newNum = desc - asc;
 
-				x = ordenDescendente(num);
-				y = ordenAscendente(num);
+                if (newNum == KAPREKAR) {
+                    break;
+                }
 
-				kapkrekar = x - y;
+                num = String.valueOf(newNum).toCharArray();
+                iterations++;
+            }
 
-				if(kapkrekar == 0) {
+            System.out.println( iterations );
+        }
 
-					iteraciones = 8;
-
-				} else {
-
-					while (kapkrekar != 6174 && iteraciones < 8) {
-
-						iteraciones++;
-
-						x = ordenDescendente(kapkrekar);
-
-						while(x < 1000) {
-							x *= 10;
-						}
-
-						y = ordenAscendente(kapkrekar);
-
-						kapkrekar = x - y;
-
-					}
-
-				}
-
-			}
-
-			System.out.println(iteraciones);
-
-		}
-
-		s.close();
-
-	}
-
-
-	// funcion que devuelve un int ordenado ascendentemente "1423" -> "1234"
-	public static int ordenAscendente(int num) {
-
-		int digito = 0;
-		int aux = 0;
-		int ordenado = 0;
-
-
-		for(int i=0; i<=9; i++) {
-
-			aux = num;
-
-			while(aux > 0) {
-
-				// recojemos el ultimo valor del numero
-				digito = aux % 10;
-
-				// comparamos el valor recojido con el valor de i para ordenarlo
-				if(digito == i) {
-					ordenado = (ordenado * 10) + digito;
-				}
-
-				aux /= 10;
-
-			}
-
-
-		}
-
-		return ordenado;
-
-	}
-
-
-	// funcion que devuelve un int ordenado desdendentemente "3412" -> "4321"
-	public static int ordenDescendente(int num) {
-
-		int digito = 0;
-		int aux = 0;
-		int ordenado = 0;
-
-		for(int i=9; i>=0; i--) {
-
-			aux = num;
-
-			while(aux > 0) {
-
-				digito = aux % 10;
-
-				if(digito == i) {
-					ordenado = (ordenado * 10) + digito;
-				}
-
-				aux /= 10;
-
-			}
-
-
-		}
-
-		return ordenado;
-
-	}
-
-
+    }
 
 }
