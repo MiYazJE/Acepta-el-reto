98393c86d618e946e8a5602c13774252acef163a
Rubén Saiz Serrano <42714332+MiYazJE@users.noreply.github.com>
2021-01-07 08:59:35 +0100

    Igualando copas

---
+++ p598.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class p589 {
+
+    public static void main(String[] args) {
+
+        final Scanner s = new Scanner(System.in);
+
+        long N, ans, max;
+
+        while (true) {
+            N = s.nextLong();
+            if (N == 0) break;
+            ans = 0L;
+            max = s.nextLong();
+            for (long i = 1; i < N; i++) {
+                long num = s.nextLong();
+                if (max < num) {
+                    ans += (num * i) - (max * i);
+                    max = num;
+                }
+                else ans += max - num;
+            }
+            System.out.println(ans);
+        }
+
+    }
+
+}
